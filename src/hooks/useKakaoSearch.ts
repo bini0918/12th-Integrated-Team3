a493971a7f448d215115fcd626b18fe26b97f7f5
Seoje1405 <hipop99@naver.com>
2025-11-19 23:54:31 +0900

    feat: Kakao 장소 키워드 검색을 위한 useKakaoSearch 훅 추가 #16

---
+++ src/hooks/useKakaoSearch.ts
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export default function useKakaoSearch() {
+  const [results, setResults] = useState<any[]>([]);
+  const [keyword, setKeyword] = useState("");
+  const [pagination, setPagination] = useState<any>(null);
+
+  const search = () => {
+    const { kakao } = window;
+    const ps = new kakao.maps.services.Places();
+
+    ps.keywordSearch(keyword, (data: any, status: any, pagination: any) => {
+      if (status === kakao.maps.services.Status.OK) {
+        setResults(data);
+        setPagination(pagination);
+      } else {
+        setResults([]);
+        setPagination(null);
+      }
+    });
+  };
+
+  return {
+    keyword,
+    setKeyword,
+    results,
+    pagination,
+    search,
+  };
+}
