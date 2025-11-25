f1a700129aaa79fa773bc58b8b5f1447a4bae277
Seoje1405 <hipop99@naver.com>
2025-11-25 12:52:30 +0900

    refactor: 매우나쁨 조건 스타일링 적용 #32

--- src/components/weather/WeatherDetail.tsx
+++ src/components/weather/WeatherDetail.tsx
@@ -1,7 +1,7 @@
 import React, { memo } from 'react';
 import { NightClouds } from '../../assets';
 import type { Location } from '../../types/location';
-import { mapCondition, getWeatherIcon } from '../../hooks/useWeatherFormat';
+import { mapCondition, getWeatherIcon } from '../../hooks/weatherHook/useWeatherFormat';
 
 interface WeatherStat {
   label: string;
@@ -25,25 +25,6 @@ export interface WeatherDisplayProps {
   tags?: WeatherTagData[];
 }
 
-// const DEFAULT_WEATHER = {
-//   date: "4월 26일",
-//   temperature: 12.2,
-//   weatherStatus: "야간 / 흐림",
-//   stats: [
-//     { label: "체감", value: "9.0", unit: "º" },
-//     { label: "습도", value: "48", unit: "%" },
-//     { label: "남동풍", value: "0.4", unit: "m/s" },
-//   ],
-//   tags: [
-//     { id: "dust", label: "미세먼지", status: "좋음" },
-//     { id: "ultra", label: "초미세먼지", status: "보통" },
-//     { id: "uv", label: "자외선", status: "위험" },
-//     { id: "sun", label: "일출", status: "05:44" },
-//   ],
-// };
-
-// 기상요소(체감, 습도, 남동풍 등등...) 보여주는 컴포넌트
-
 const StatItem = memo(({ label, value, unit }: WeatherStat) => (
   <div className="flex items-center gap-1 text-gray-600 font-medium text-sm md:text-base">
     <span className="text-gray-500">{label}</span>
@@ -54,8 +35,6 @@ const StatItem = memo(({ label, value, unit }: WeatherStat) => (
   </div>
 ));
 
-//좋음, 보통, 나쁨에 따라 색을 바꾸는 컴포넌트
-
 const StatusTag = memo(({ label, status }: WeatherTagData) => {
   const bgColor =
     status === '좋음'
@@ -64,9 +43,11 @@ const StatusTag = memo(({ label, status }: WeatherTagData) => {
         ? 'bg-[#CEFFCC]'
         : status === '나쁨' || status === '위험'
           ? 'bg-[#FFCCCC]'
-          : status.includes(':')
-            ? 'bg-[#F6FFCC]'
-            : 'bg-gray-200';
+          : status === '매우나쁨'
+            ? 'bg-[#FF9999]'
+            : status.includes(':')
+              ? 'bg-[#F6FFCC]'
+              : 'bg-gray-200';
 
   const textColor =
     status === '좋음'
@@ -75,9 +56,11 @@ const StatusTag = memo(({ label, status }: WeatherTagData) => {
         ? 'text-[#32FF35]'
         : status === '나쁨' || status === '위험'
           ? 'text-[#FF3232]'
-          : status.includes(':')
-            ? 'text-[#FFC532]'
-            : 'text-black';
+          : status === '매우나쁨'
+            ? 'text-[#FF0000]'
+            : status.includes(':')
+              ? 'text-[#FFC532]'
+              : 'text-black';
 
   return (
     <div
